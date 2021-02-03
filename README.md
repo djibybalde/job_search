61fff3a1224a4dc146add8d8b786597b64cff813
Djiby BALDE <dybalde@gmail.com>
2021-02-03 16:54:21 +0100

    Updated files

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 
 [Job Search](https://github.com/djibybalde/job_search) 
 ==========
+
 Build a [`Job Finder`](https://github.com/djibybalde/job_search) to scrape jobs on https://fr.indeed.com website.
 
 
 External tool
 =============
-This material uses `ChromeDriver` to run and control `Google Chrome browser`. So, you should have `Chrome navigator` in your computer and download [`ChromeDriver`](https://chromedriver.chromium.org) from [here](https://chromedriver.chromium.org).   
-After downloading [ChromeDriver](https://chromedriver.chromium.org), put it in the `references` folder with the name `chromedriver`. 
+This material uses [`ChromeDriver`](https://chromedriver.chromium.org) to run and control `Google Chrome browser`. 
+So, make sur that you have `Chrome navigator` in your computer or download it. 
+If your are using MacOS, you don't have to download [ChromeDriver](https://chromedriver.chromium.org). 
+Otherwise, download it from [here](https://chromedriver.chromium.org) and put it in the `references` folder with the name `chromedriver`. 
+
 
 How to use this [`Job Finder`](https://github.com/djibybalde/job_search)
 ============
@@ -20,37 +24,31 @@ $ cd job_search
 $ pip install pipenv
 $ pipenv install
 ```
-
 or 
 ```bash
 $ cd job_search
 $ pip install job_search
 ```
 
-- Enter your [fr.indeed.com](https://fr.indeed.com) `E-mail` and `password`, when instructed.
-```
-Enter your E-mail address here: my_email@example.com
-Enter your password here: ********
-```
-
+- After installation, run the following to scrape your job and apply it. 
 ```bash
 $ python
 >>> from indeed import JobFinder
 >>> JobFinder().get_results()
 ```
 
-You can specifier some arguments:
-- `job_title[OPTIONAL]`: the title of the job you are looking for,
-- `location[OPTIONAL]`: your favorite location or address, 
-- `job_type[OPTIONAL]`: `CDI` or `CDD`
-- `length[OPTIONAL]`: the number of jobs to scrape and write in the `data`
-
-For example
+- You can also specifier some arguments:
+- `login[OPTIONAL]`: Whether to login with your email and password on the website.  
+- `job_title[OPTIONAL]`: What job are you looking for ? Default is `Data Science` 
+- `location[OPTIONAL]`: Where do you want to apply for job ? For example Paris, Lyon.
+- `max_numb[OPTIONAL]`: How many example of job do you want to apply ? Default is 20. 
 
 ```bash
 $ python
 >>> from indeed import JobFinder
->>> JobFinder(job_title='Data Science', location='Lyon', length=10).get_results()
+>>> JobFinder(login=True, job_title='Data Science', location='Lyon', max_numb=10).get_results()
 ```
 
-Enjoy with [Job Search](https://github.com/djibybalde/job_search) and **good Luck!**
+- ***Note that*** if you set `login=True`, your [fr.indeed.com](https://fr.indeed.com) `email` and `password` will be asked to login.
+
+**Enjoy with [Job Search](https://github.com/djibybalde/job_search) and *good Luck!***
--- src/indeed.py
+++ src/indeed.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-"""
-    Scripts to search and scrape jobs on https://fr.indeed.com
-"""
+""" Scripts to search and scrape jobs on https://fr.indeed.com """
 
 import os
-import yaml
 import time
+import yaml
+import json
 import numpy as np
 import pandas as pd
 from termcolor import colored
+from nltk.tokenize import word_tokenize
 
 from bs4 import BeautifulSoup
 
@@ -17,21 +17,31 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
+# Yaml file to get directories/paths
 ENV_FILE = os.path.join(os.getcwd().split('job_search')[0], 'job_search', 'env.yaml')
 with open(ENV_FILE) as yf:
-    params = yaml.load(yf, Loader=yaml.FullLoader)
+    PARAMS = yaml.load(yf, Loader=yaml.FullLoader)
 
 # Directories initialisation
 ROOT_DIR = os.path.dirname(os.path.abspath(ENV_FILE))
-DRIVER_PATH = os.path.join(ROOT_DIR, params['directories']['driver_dir'], params['files']['driver_path'])
-OUTPUT_PATH = os.path.join(ROOT_DIR, params['directories']['scraped'])
+DRIVER_PATH = os.path.join(ROOT_DIR, PARAMS['directories']['driver_dir'], PARAMS['files']['driver_path'])
+FILTER_FILE = os.path.join(ROOT_DIR, PARAMS['directories']['data_dir'], PARAMS['files']['filter_word'])
+OUTPUT_PATH = os.path.join(ROOT_DIR, PARAMS['directories']['job_data'])
 
+# Make an output path
 if not os.path.exists(OUTPUT_PATH):
     os.makedirs(OUTPUT_PATH, exist_ok=True)
 
 
 def timer(t1=1, t2=2):
+    """ Return a random number between t1 and t2"""
     return np.random.uniform(t1, t2)
+
+
+def read_json(path: str):
+    """ Read a JSON file """
+    with open(path, mode='r') as file:
+        return json.loads(file.read())
 
 
 def params(email: str = '', pwd: str = None) -> tuple:
@@ -56,135 +66,134 @@ def params(email: str = '', pwd: str = None) -> tuple:
 
 
 class JobFinder:
-    """
-    Search and scrape job from fr.indeed.com
-    """
+
+    """ Search and scrape job from https://fr.indeed.com website """
 
     def __init__(self,
-                 user_params: tuple = params(),
-                 job_title: str = 'data scientist',
+                 login: bool = False,
+                 job_title: str = 'data science',
                  location: str = 'france',
                  job_type: str = 'cdi',
-                 length: int = 10,
+                 max_numb: int = 20,
                  ):
         """
         Parameters initialisation
 
         Args:
-            user_params (tuple): Your email (address, password) respectively
-            job_title (str): What is your favorite job title or keyword ?
-            location (str): Where do you want to apply the for job ?
-            job_type (str): Which type of job are you looking for ?
-            length (str): How many example of job do you want to apply ?
+            login (bool): Whether to login with your email and password on the website.
+            job_title (str): What job are you looking for ?
+            job_type (str): What type of job are you looking for e.g. CDI, CDD ?
+            location (str): Where do you want to apply for job ?
+            max_numb (str): How many example of job do you want to apply ?
 
         """
 
-        self.driver_path = DRIVER_PATH
-        print(self.driver_path)
-        self.email, self.password = user_params
+        self.log_in = login
+        self.email, self.password = params() if self.log_in else '', ''
+
         self.job_title = job_title.lower()
         self.job_type = job_type.lower()
         self.location = location.capitalize()
-        self.length = int(length)
+        self.max_numb = int(max_numb)
 
-        self.driver = webdriver.Chrome(executable_path=self.driver_path, options=webdriver.ChromeOptions())
+        self.excludes = read_json(FILTER_FILE)['excludes']
+        self.includes = read_json(FILTER_FILE)['includes']
 
-        self.excludes = ['stage', 'stagiaire', 'internship', 'apprentissage', ' contrat pro', 'alternance',
-                         'alternance/stage', 'stage/alternance', '(alternance/stage)', '(stage/alternance)',
-                         'intern', '[stage]', '[alternance]', '(alternance)', '(stage)',
-                         'lead', 'senior', 'confirmé', 'expert', 'expérimenté', 'chef', 'Chief', ]
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=webdriver.ChromeOptions())
 
     def login(self):
 
-        """
-        Login into fr.indeed.com
-        """
+        """ Login into fr.indeed.com website """
+
         self.driver.set_window_size(width=1000, height=900)  # maximize_window()
         self.driver.get('https://fr.indeed.com')
-
-        print(f"\nLogin into {colored('https://fr.indeed.com', 'blue')} ...")
         time.sleep(timer(1, 2))
-        self.driver.find_element_by_class_name('gnav-LoggedOutAccountLink-text').click()
 
+        if self.log_in:
+            print(f"\nLogin into {colored('https://fr.indeed.com', 'blue')} ...")
+            self.driver.find_element_by_class_name('gnav-LoggedOutAccountLink-text').click()
+            time.sleep(timer(1, 2))
+
+            email = self.driver.find_element_by_id('login-email-input')
+            email.clear()
+            email.send_keys(self.email)
+            time.sleep(timer(1, 2))
+
+            password = self.driver.find_element_by_id('login-password-input')
+            password.clear()
+            password.send_keys(self.password)
+            time.sleep(timer(2, 3))
+
+            password.send_keys(Keys.RETURN)
+
+    def search(self, select_type: bool = False):
+
+        """ Searching for jobs """
+
+        # Open https://fr.indeed.com in the Google Chrome browser and login.
+        self.login()
         time.sleep(timer(1, 2))
-        email = self.driver.find_element_by_id('login-email-input')
-        email.clear()
-        email.send_keys(self.email)
-
-        time.sleep(timer(1, 2))
-        password = self.driver.find_element_by_id('login-password-input')
-        password.clear()
-        password.send_keys(self.password)
-
-        time.sleep(timer(2, 3))
-        password.send_keys(Keys.RETURN)
-
-    def search(self):
-        """
-        Searching for jobs
-        """
 
         print(f"Searching for {colored(self.job_title.capitalize(), 'blue')} job ...")
-        time.sleep(timer(1, 2))
+
+        # What kind of job are you for ?
         keywords = self.driver.find_element_by_id("text-input-what")
         keywords.click()
         keywords.clear()
         keywords.send_keys(self.job_title)
-
         time.sleep(timer(1, 2))
+
+        # Where do want to apply for job ?
         location = self.driver.find_element_by_id("text-input-where")
         location.send_keys(len(location.get_attribute('value')) * Keys.BACK_SPACE)
         location.send_keys(self.location)
         location.send_keys(Keys.RETURN)
+        time.sleep(timer(2, 3))
 
-        time.sleep(timer(1, 2))
-        self.driver.find_element_by_id("filter-job-type").click()
-
-        if self.job_title.lower() is 'data science':
-            job_type = '5' if self.job_type == 'cdi' else '7'
-
-        elif self.job_title.lower() is 'data scientist':
-            job_type = '1' if self.job_type == 'cdi' else '5'
-
-        elif self.job_title.lower() is 'data analyst':
-            job_type = '2' if self.job_type == 'cdi' else '7'
-
-        else:  # self.job_title.lower() in ['data analyse', 'statistique', 'statisticien']:
-            job_type = '2' if self.job_type == 'cdi' else '4'
-
-        job_type = f"//*[@id='filter-job-type-menu']/li[{job_type}]"
-        self.driver.find_element_by_xpath(job_type).click()
-
-        time.sleep(timer(1, 2))
-        button_x = self.driver.find_element_by_id("popover-x")
-        button_x.click()
-
-        time.sleep(timer(1, 2))
+        # Accept the cookies
         cookies = self.driver.find_element_by_id("onetrust-accept-btn-handler")
         cookies.click()
+        time.sleep(timer(1, 2))
 
-        try:
-            self.driver.find_element_by_xpath("//*[@id='filter-dateposted']/button").click()
-            time.sleep(timer(0, 1))
-            self.driver.find_element_by_xpath("//*[@id='filter-dateposted']/button").click()
-            time.sleep(timer(0, 1))
-            self.driver.find_element_by_xpath("//*[@id='filter-dateposted-menu']/li[2]").click()
+        # Select the publication date
+        self.driver.find_element_by_id("filter-dateposted").click()
+        self.driver.find_element_by_xpath("//*[@id='filter-dateposted']/button").click()
+        self.driver.find_element_by_xpath("//*[@id='filter-dateposted-menu']/li[2]").click()
+        time.sleep(timer(1, 2))
 
-        except NoSuchElementException:
-            pass
+        # Close the popup window: appear when the page is refresh
+        button_x = self.driver.find_element_by_id("popover-x")
+        button_x.click()
+        time.sleep(timer(1, 2))
+
+        # Select job type (CDI, CDD, internship, ...)
+        if select_type:
+            self.driver.find_element_by_id("filter-job-type").click()
+
+            if self.job_title is 'data science':
+                job_type = '5' if self.job_type == 'cdi' else '7'
+            elif self.job_title is 'data scientist':
+                job_type = '1' if self.job_type == 'cdi' else '5'
+            elif self.job_title is 'data analyst':
+                job_type = '2' if self.job_type == 'cdi' else '7'
+            else:
+                job_type = '2' if self.job_type == 'cdi' else '4'
+
+            job_type = f"//*[@id='filter-job-type-menu']/li[{job_type}]"
+            self.driver.find_element_by_xpath(job_type).click()
+            time.sleep(timer(1, 2))
 
     def scrape(self):
-        """
-        Jobs scraper
-        """
+
+        """ Jobs scraper """
 
         page = 0
         jobs = list()
 
         print(f"Scraping {colored(self.job_title.capitalize(), 'blue')} jobs ...")
-        while len(jobs) < self.length:
+        while len(jobs) < self.max_numb:
             count = 0
-            print(f' - Scraping in page {1 + page} ... ')
+            print(' - Scraping in page {} ... '.format(1 + page))
 
             current_page = self.driver.page_source
             soup = BeautifulSoup(current_page, "lxml")
@@ -193,11 +202,11 @@ class JobFinder:
             for job in jobs_container.find_all('div', class_='jobsearch-SerpJobCard'):
 
                 title_ref = job.find('h2', class_="title").text.strip()
-                if all(exclude not in title_ref.lower().split() for exclude in self.excludes):
+                if all(word not in word_tokenize(title_ref.lower(), language='french') for word in self.excludes):
 
-                    title = ' '.join([t for t in title_ref.split() if t != 'nouveau'])
-                    company = job.find('span', class_='company').text.strip()
                     link = "https://fr.indeed.com" + job.find('a').get("href")
+                    title = ' '.join([w for w in title_ref.split() if w != 'nouveau'])
+                    company = job.find('span', class_='company').text.strip()
 
                     if job.find('span', class_='date') is not None:
                         date = job.find('span', class_='date').text.strip()
@@ -213,10 +222,8 @@ class JobFinder:
                     jobs.append({'Title': title, 'Company': company, 'Location': location, 'Date': date, 'Link': link})
 
                 count += 1
-                if len(jobs) >= self.length:
+                if len(jobs) >= self.max_numb:
                     break
-
-            print(f' - Found: {count} ==> total: {len(jobs)} ')
 
             # Next page
             page += 1
@@ -226,24 +233,36 @@ class JobFinder:
             except NoSuchElementException:
                 pass
 
-        # Ignore the duplicated data
-        df = pd.DataFrame(jobs).drop_duplicates(ignore_index=True)
-        template = colored('Found {} {} unique jobs (no-duplicated).', 'green')
-        print(template.format(df.shape[0], self.job_title.capitalize()))
+        # Ignore the duplicated data and write to a Excel file
+        if all(n in word_tokenize(self.job_title, language='french')
+               for n in word_tokenize("chargé d'études statistiques", language='french')):
+            NAME = 'Statistiques'
+        else:
+            NAME = ''.join(word.capitalize() for word in self.job_title.split())
 
-        # Write to a Excel file
-        MY_FILE_NAME = ''.join([OUTPUT_PATH, ''.join(self.job_title.split()), '.xlsx'])
-        df.to_excel(MY_FILE_NAME)
-        print(colored(f'Your data are written in {MY_FILE_NAME}', 'green'))
+        df = pd.DataFrame(jobs).drop_duplicates(ignore_index=True)
+        template = colored('Found {} {} jobs (no-duplicated)', 'cyan')
+        print(template.format(df.shape[0], NAME))
+
+        FILE_LOC = ''.join([OUTPUT_PATH, NAME, '.xlsx'])
+        df.to_excel(FILE_LOC)
+        print(f"This data are written in {colored(FILE_LOC, 'cyan')}")
 
         # Close the driver
         self.driver.quit()
 
     def get_results(self):
-        self.login()
-        self.search()
+        self.search(select_type=False)
         self.scrape()
 
 
 if __name__ == '__main__':
-    JobFinder(job_title='data science', location='france', length=50, ).get_results()
+    JobFinder(login=False, job_title='data scientist', location='france', max_numb=50, ).get_results()
+    print(' ')
+    time.sleep(timer(2, 4))
+
+    JobFinder(login=False, job_title="data analyst", location='france', max_numb=20, ).get_results()
+    print(' ')
+    time.sleep(timer(2, 4))
+
+    JobFinder(login=False, job_title="chargé d'études statistiques", location='france', max_numb=30, ).get_results()
